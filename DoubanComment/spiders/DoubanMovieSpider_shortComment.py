4a27b9f1a1b07490b4f6c8b603e163f30309c8d0
zhaphotons <zhaphotons@gmail.com>
2016-09-13 14:50:28 +0800

    rewrite spider to directly gather data

--- DoubanComment/spiders/DoubanMovieSpider_shortComment.py
+++ DoubanComment/spiders/DoubanMovieSpider_shortComment.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 13 14:04:59 2016
+
+@author: lenovo
+"""
+
 # encoding=utf-8
 import scrapy
 import logging
 import time
-import re
 import codecs
 from scrapy.shell import inspect_response
 
@@ -11,7 +17,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 class DoubanMovieSpider(scrapy.Spider):
-    name = "DoubanMovie_longComment"
+    name = "DoubanMovie_shortComment"
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36",
@@ -29,7 +35,7 @@ class DoubanMovieSpider(scrapy.Spider):
 
     def login(self, response):
         formdata={
-             "redir": "https://movie.douban.com/subject/2131940/reviews?start=0&sort=time",
+             "redir": "https://movie.douban.com/subject/26776092/comments?start=0&limit=20&sort=new_score",
              "form_email": "ravna@qq.com",
              "form_password": "huapanda2013",
              "login": u"登录",
@@ -52,39 +58,21 @@ class DoubanMovieSpider(scrapy.Spider):
     def parse(self, response):
         # assert response.css(".nav-user-account").extract(), "not login"
 #        inspect_response(response, self)
-    
-        time.sleep(0.5)
-        
-        urls = response.xpath('//div[@class="middle"]//h3/a/@href').extract()
+        sel = scrapy.Selector(response)
 
-        for url in urls:
+        results = sel.xpath('//div[@class="comment-item"]')
+
+        for re in results:
             item = {}
 
-#            time.sleep(0.5)
-            
-            request = scrapy.Request(url, headers=self.headers, callback=self.parse_Content)
-            request.meta['item'] = item
-            
-            yield request
-       
-        for href in response.xpath('//div[@class="paginator"]/span[@class="next"]/link/@href').extract():
-            full_url = response.urljoin(href)
+            item['comment'] = re.xpath('./div[@class="comment"]/p/text()').extract()[0]
+            try:
+                item['rating'] = re.xpath('./div[@class="comment"]/h3/span[2]/span[1]/@class').extract()[0][7]
+            except:
+                item['rating'] = 'None'
+
+            yield item
+
+        for i in response.css("#paginator .next").xpath("@href").extract():
+            full_url = response.urljoin(i)
             yield scrapy.Request(full_url, headers=self.headers, callback=self.parse)
-        
-        
-    def parse_Content(self, response):
-        
-#        time.sleep(1.0)
-#        inspect_response(response, self)
-        
-        item = response.meta['item']
-        
-        try:
-            item['rating'] = response.xpath('//header[@class="main-hd"]/span[1]/@class').extract()[0][7]        
-        except:
-            item['rating'] = 'None'
-        
-        pars = response.xpath('//div[@class="main-bd"]//div[@class="clearfix"]//text()').extract()
-        item['comment'] = reduce(lambda x,y: x+y, pars).strip()
-        
-        yield item
